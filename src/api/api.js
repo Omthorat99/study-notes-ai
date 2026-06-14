8f7a51ba8e6f59e0bea4f5b75e469e20a89c0e43
Om Thorat <thoratom21@gmail.com>
2026-06-14 23:52:52 +0530

    Update production API URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://study-notes-ai.onrender.com",
 });
 
 API.interceptors.request.use(
